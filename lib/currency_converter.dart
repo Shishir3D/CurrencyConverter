a8e341ea35b5cb315c0ff8dd0c253f8add4e51b3
shishir3d <shishirpoudel2061@gmail.com>
2024-07-02 21:29:30 +0545

    update : on button press print text from textbox on the console

--- lib/currency_converter.dart
+++ lib/currency_converter.dart
@@ -5,28 +5,73 @@ class CurrencyConverter extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final TextEditingController textEditingController = TextEditingController();
+    const result = "12";
+    final border = OutlineInputBorder(
+      borderSide: const BorderSide(
+        width: 2.0,
+        style: BorderStyle.solid,
+      ),
+      borderRadius: BorderRadius.circular(20),
+    );
+
+    return Scaffold(
       backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: const Text(
+          "Currency Converter",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 30,
+          ),
+        ),
+        foregroundColor: Colors.white,
+        centerTitle: true,
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              "0",
+            const Text(
+              result,
               style: TextStyle(
                   color: Colors.white,
                   fontSize: 50,
                   fontWeight: FontWeight.bold),
             ),
-            TextField(
-              style: TextStyle(
-                fontSize: 30,
-                color: Colors.white,
+            Container(
+              padding: const EdgeInsets.all(20),
+              child: TextField(
+                controller: textEditingController,
+                style: const TextStyle(
+                  fontSize: 30,
+                  color: Colors.black,
+                ),
+                decoration: InputDecoration(
+                  hintText: "NPR to USD",
+                  prefixIcon: const Icon(Icons.monetization_on),
+                  prefixIconColor: Colors.green,
+                  filled: true,
+                  fillColor: Colors.white,
+                  focusedBorder: border,
+                  enabledBorder: border,
+                ),
+                keyboardType: TextInputType.number,
               ),
-              decoration: InputDecoration(
-                hintText: "NPR to USD",
-                prefixIcon: Icon(Icons.monetization_on),
-                prefixIconColor: Colors.black,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 80),
+              child: TextButton(
+                onPressed: () {
+                  debugPrint(textEditingController.text);
+                },
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                  minimumSize: const Size(double.infinity, 50),
+                ),
+                child: const Text("Click me"),
               ),
             ),
           ],
