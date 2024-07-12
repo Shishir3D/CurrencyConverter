95a0fca6d3805e221415311e3bb625813c3ae394
shishir3d <shishirpoudel2061@gmail.com>
2024-07-12 22:45:04 +0545

    [CHANGE] : completed the cupertino and material app

--- lib/currency_converter_cupertino.dart
+++ lib/currency_converter_cupertino.dart
@@ -1,84 +1,70 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
-class CurrencyConverter extends StatefulWidget {
-  const CurrencyConverter({super.key});
+class CurrencyConverterCupertino extends StatefulWidget {
+  const CurrencyConverterCupertino({super.key});
 
   @override
-  State<CurrencyConverter> createState() =>
-      _CurrencyConverterMaterialStatePage();
+  State<CurrencyConverterCupertino> createState() =>
+      _CurrencyConverterCupertinoState();
 }
 
-class _CurrencyConverterMaterialStatePage extends State<CurrencyConverter> {
+class _CurrencyConverterCupertinoState
+    extends State<CurrencyConverterCupertino> {
   final TextEditingController textEditingController = TextEditingController();
   double result = 0;
   @override
   Widget build(BuildContext context) {
-    final border = OutlineInputBorder(
-      borderSide: const BorderSide(
-        width: 2.0,
-        style: BorderStyle.solid,
-      ),
-      borderRadius: BorderRadius.circular(20),
-    );
-
-    return Scaffold(
+    return CupertinoPageScaffold(
       backgroundColor: const Color.fromARGB(255, 39, 39, 39),
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 39, 39, 39),
-        title: const Text(
+      navigationBar: const CupertinoNavigationBar(
+        backgroundColor: Color.fromARGB(255, 39, 39, 39),
+        middle: Text(
           "Currency Converter",
           style: TextStyle(
             fontWeight: FontWeight.bold,
             fontSize: 30,
+            color: CupertinoColors.white,
           ),
         ),
-        foregroundColor: Colors.white,
-        centerTitle: true,
       ),
-      body: Center(
+      child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
               '\$ ${result > 10000000 ? "😒" : result != 0 ? result.toStringAsFixed(2) : result.toStringAsFixed(0)}',
               style: const TextStyle(
-                color: Colors.white,
+                color: CupertinoColors.white,
                 fontSize: 50,
                 fontWeight: FontWeight.bold,
               ),
             ),
             Container(
               padding: const EdgeInsets.all(20),
-              child: TextField(
+              child: CupertinoTextField(
                 controller: textEditingController,
                 style: const TextStyle(
                   fontSize: 30,
-                  color: Colors.black,
+                  color: CupertinoColors.black,
                 ),
-                decoration: InputDecoration(
-                  hintText: "NPR to USD",
-                  prefixIcon: const Icon(Icons.currency_rupee_outlined),
-                  prefixIconColor: Colors.green,
-                  filled: true,
-                  fillColor: Colors.white,
-                  focusedBorder: border,
-                  enabledBorder: border,
+                decoration: BoxDecoration(
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(5),
+                  color: CupertinoColors.white,
                 ),
+                placeholder: "Enter amount in NPR",
+                prefix: const Icon(CupertinoIcons.money_dollar_circle),
                 keyboardType: TextInputType.number,
               ),
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 80),
-              child: TextButton(
+              child: CupertinoButton(
+                color: CupertinoColors.black,
                 onPressed: () {
                   result = double.parse(textEditingController.text) * 133.63;
                   setState(() {});
                 },
-                style: TextButton.styleFrom(
-                  backgroundColor: const Color.fromARGB(255, 4, 4, 4),
-                  foregroundColor: Colors.white,
-                  minimumSize: const Size(double.infinity, 50),
-                ),
                 child: const Text("Convert"),
               ),
             ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:firstapp/currency_converter.dart';
+import 'package:firstapp/currency_converter_cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() {
   runApp(const MyApp());
+  //change 'MyApp()' to 'MyCupertinoApp()' to switch to cupertino design
 }
 
 class MyApp extends StatelessWidget {
@@ -12,6 +15,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: CurrencyConverter(),
+    );
+  }
+}
+
+class MyCupertinoApp extends StatelessWidget {
+  const MyCupertinoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoApp(
+      home: CurrencyConverterCupertino(),
     );
   }
 }
